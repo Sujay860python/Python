18f648181e029ac5926e234d4ce31924541f842d
Sujay860python <sujaysingh0713@gmail.com>
2026-05-08 20:39:33 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age: "))
+if age <=20:
+   print("You are allowed.")
+else:
+   age=int(input("Enter the age: "))
+   if age==20:
+      print("Allowed")
+   else:
+      print("Not Allowed")
