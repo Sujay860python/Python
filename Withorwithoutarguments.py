0a2ae999f0ea1eeb6c1467cf130d4955d2df2eea
Sujay860python <sujaysingh0713@gmail.com>
2026-05-21 13:06:53 +0530

    Add files via upload

---
+++ Withorwithoutarguments.py
@@ -0,0 +1,16 @@
+#Code1
+def add(a,b):
+    print(a+b)
+add(3,6)
+
+#Code2
+def sub(x,y):
+    print(x-y)
+sub(7,4)
+
+#Code3
+def today_date():
+    print("Do you know what is today's date?")
+    print("Ya!today's date is 21/5/2026.")
+today_date()
+
