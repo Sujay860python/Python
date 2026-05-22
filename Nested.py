7458c87a46553dccf9eb1778079c2c64e31377eb
Sujay860python <sujaysingh0713@gmail.com>
2026-05-22 11:14:55 +0530

    Add files via upload

---
+++ Nested.py
@@ -0,0 +1,11 @@
+medical_cause=input("Did you have a medical cause (Y/N:)").strip().upper()
+if medical_cause=='Y':
+    print("You are Allowed")
+else:
+    atten=int(input("Enter the attendance:"))
+    if atten >=75:
+        print("Allowed")
+    else:
+        print("Not Allowed")
+        
+              
