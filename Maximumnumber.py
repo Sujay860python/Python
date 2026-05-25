7ed8d793f2ec58edfc9d2577a7bf89182d870cb6
Sujay860python <sujaysingh0713@gmail.com>
2026-05-25 20:06:17 +0530

    Add files via upload

---
+++ Maximumnumber.py
@@ -0,0 +1,10 @@
+def print_large(x,y,z):
+    large=max(x,y,z)
+    print("The largest number is :"  ,large)
+
+#Usage Example
+print_large(29,87,86)
+
+
+
+
---
+++ Oddevenarguments.py
@@ -0,0 +1,9 @@
+def check_even_odd(number):
+    if number % 2 == 0 :
+        print("It is a Even Number.")
+    else:
+        print("It is a odd number.")
+
+#Example Usage 
+check_even_odd(7)
+check_even_odd(4)
