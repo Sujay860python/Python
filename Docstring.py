35308d11079de73f625d138e59b357b0faa5f745
Sujay860python <sujaysingh0713@gmail.com>
2026-05-25 17:39:43 +0530

    Add files via upload

---
+++ Docstring.py
@@ -0,0 +1,14 @@
+def sub (x,y):
+    """The two numbers we have to subtract"""
+    print(x-y)
+sub(6,4)
+
+def student_user(name,ID):
+    print("name is :" ,name,"and ID is :" ,ID)
+student_user("Pankaj",99)
+student_user("Rahul",89)
+student_user("Sujay",79)
+student_user("Kiran",98)
+student_user("Mika",69)
+
+
