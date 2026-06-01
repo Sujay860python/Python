bd7a83900e08333e677e76cd03b6588852d32012
Sujay860python <sujaysingh0713@gmail.com>
2026-06-01 18:07:28 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,38 @@
+#1.print favourite fruit 4 times
+i=1
+
+while i<=4:
+    print("Mango🥭!!")
+    i+=1
+
+#2.print numbers from(1-20)
+
+i=1
+
+while i<=20:
+    print(i)
+    i+=1
+
+#3.print even numbers from (2-20)
+
+i=2
+
+while i<=20:
+    print(i)
+    i+=2
+
+#4.print numbers from 10 to 1
+i=10
+
+while i >=1:
+    print(i)
+    i-=1
+
+
+#5.print name 10 times
+
+i=1
+
+while i<=10:
+    print("Sujay")
+    i+=1
