70bce9cb37c0bb600fb7ed12e292710c135a83d6
Sujay860python <sujaysingh0713@gmail.com>
2026-06-22 16:44:24 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,28 @@
+#List
+fruits=["Apple","Banana","Mango"]
+print(fruits)
+
+print(fruits[1])
+
+fruits.pop(1)
+print(fruits)
+
+fruits.append("Dragon fruit")
+print(fruits)
+
+
+fruits.append("Orange")
+print(fruits)
+
+
+#Shopping List
+
+shopping=["Bread","Milk","Eggs"]
+
+
+shopping.pop(0)
+
+shopping.append("Butter")
+print(shopping)
+
+
