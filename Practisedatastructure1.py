5592b7bc64297906a8a771a3f1c09dafaacd3f9d
Sujay860python <sujaysingh0713@gmail.com>
2026-06-25 17:36:49 +0530

    Add files via upload

---
+++ Practisedatastructure1.py
@@ -0,0 +1,31 @@
+#1. Create a list of 5 fruits and print the list.
+#Answer-
+fruits_print=["Apple","Mango","Banana","Orange","Dragonfruit"]
+print( fruits_print)
+
+#2. Create a list of numbers [10, 20, 30, 40, 50] and print the first element.
+#Answer-
+num_first=["10", "20", "30", "40", "50"]
+print(num_first[0])
+
+#3. Create a list of colors and print the last element.
+#Answer-
+color_last=["Red","Blue","Green","White","Black"]
+print(color_last[4])
+
+#4. Add "Mango" to the list ["Apple", "Banana", "Orange"].
+#Answer-
+fruits_add= ["Apple", "Banana", "Orannge"]
+fruits_add.append("Mango")
+print(fruits_add)
+
+#5. Remove "Banana" from the list ["Apple", "Banana", "Orange"].
+#Answer-
+fruits_pop=["Apple", "Banana", "Orange"]
+fruits_pop.pop("Banana")
+print(fruits_pop)
+
+#6. Find the length of the list ["Red", "Blue", "Green", "Yellow"].
+#Answer-
+color_length= ["Red", "Blue", "Green", "Yellow"]
+print(len(color_length))
---
+++ Tuple.py
@@ -0,0 +1,9 @@
+#Tuple
+fruits=("Apple","Banana","Mango")
+print(fruits)
+
+print(len(fruits))
+
+print(fruits[1])
+print(fruits[0])
+print(fruits[-1])
