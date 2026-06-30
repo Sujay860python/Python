5566e676ed9bafc399183903cba780f1fbb8c756
Sujay860python <sujaysingh0713@gmail.com>
2026-06-30 21:53:45 +0530

    Add files via upload

---
+++ Practisedatastructurepart2.py
@@ -0,0 +1,53 @@
+#Create a list of 5 student names and display them.
+#Answer:-
+students_name=["Ramesh","Suresh","Sujay","Mayank","Rohan"]
+print(students_name)
+               
+#Create a tuple of 7 days of the week and display all days.
+#Answer:-
+week_days=("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+print(week_days)
+
+#Create a dictionary containing a student's Name, Age, and Class.
+#Answer:-
+student_identity={
+     "student":"Sujay",
+     "age":13,
+     "class":"8th"
+}
+#Create a tuple of 5 colors and display the first color.
+#Answer:-
+colors_display=("Red","Blue","White","Green","Black")
+print(colors_display[1])
+
+#Create a dictionary of 3 countries and their capitals.
+#Answer:-
+country_capital={
+     "India":"New Delhi",
+     "Japan":"Tokyo",
+     "Nepal":"Kathmandu"
+}
+print(country_capital)
+#Add a new item to a list and display the updated list.
+#Find the length of a tuple containing 10 numbers.
+#Answer:-
+num_count=(56,78,89,23,58,89,78,67,34,66)
+
+print(len(num_count))
+
+
+#Add a new key-value pair to a dictionary.
+#Answer:-
+student_result={
+    "student name":"Sujay Singh",
+    "class":"10th",
+    "school name":"O.P.Jinal School"
+}
+student_result["result"]="99 marks"
+print(student_result)
+
+#remove an item from a list.
+#Answer:-
+season_remove=["Autumn","Spring","Summer","Winter"]
+season_remove.pop(1)
+print(season_remove)
