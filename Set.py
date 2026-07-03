099c84ba939b6f03252d233047f49e16f9a95bd6
Sujay860python <sujaysingh0713@gmail.com>
2026-07-03 19:22:55 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,19 @@
+#Q1. Create a set of student IDs and print all IDs.
+#Answer:-
+student_id={"Rahul","tenth",15,+919875673456}
+print(student_id)
+
+#Q2. Add a new student ID to the set.
+#Answer:-
+student_id.add("Samir")
+print(student_id)
+
+#Q3. Remove a student ID from the set.
+#Answer:-
+student_id.remove("Rahul")
+print(student_id)
+
+#Q4. Print all elements of a set using a loop.
+#Answer:-
+for i in student_id :
+        print(i)
